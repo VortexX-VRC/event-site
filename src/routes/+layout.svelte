9846e79b69c23e0c986173fff8d22525d19bb8b1
Francis Ng <github@fnha.anonaddy.com>
2026-05-15 17:33:54 +0900

    WIP

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,51 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher, setMode } from 'mode-watcher';
+	import * as NavigationMenu from '$lib/components/ui/navigation-menu/index.js';
+	import logo from '$lib/assets/logo.webp';
 
 	let { children } = $props();
 	setMode('dark');
+	const navHover = `w-36 justify-center transition bg-linear-to-b hover:from-fuchsia-500 hover:to-purple-800 hover:bg-fuchsia-800]`;
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <ModeWatcher />
+<NavigationMenu.Root class="py-1 w-full max-w-full fixed bg-[url(/images/navbar.svg)] backdrop-blur-lg z-10">
+  <div class="nav-logo me-2"><enhanced:img src={logo} /></div>
+  <NavigationMenu.List>
+    <NavigationMenu.Item>
+      <NavigationMenu.Link class={navHover}>
+        <a href="#story">ストーリ</a>
+      </NavigationMenu.Link>
+    </NavigationMenu.Item>
+    <NavigationMenu.Item>
+      <NavigationMenu.Link class={navHover}>
+        <a href="#info">イベント情報</a>
+      </NavigationMenu.Link>
+    </NavigationMenu.Item>
+    <NavigationMenu.Item>
+      <NavigationMenu.Link class={navHover}>
+        <a href="#members">メンバー</a>
+      </NavigationMenu.Link>
+    </NavigationMenu.Item>
+    <NavigationMenu.Item>
+      <NavigationMenu.Link class={navHover}>
+        <a href="#videos">動画</a>
+      </NavigationMenu.Link>
+    </NavigationMenu.Item>
+    <NavigationMenu.Item>
+      <NavigationMenu.Link class={navHover}>
+        <a href="#gallery">ギャラリー</a>
+      </NavigationMenu.Link>
+    </NavigationMenu.Item>
+  </NavigationMenu.List>
+</NavigationMenu.Root>
 {@render children()}
+
+<style>
+.nav-logo img {
+  width: 4rem;
+  height: auto;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,47 @@
 <script lang="ts">
+  import logo from '$lib/assets/logo.webp';
+
   let { data } = $props();
 </script>
 
-<div class="w-full h-screen bg-linear-65 from-purple-500 to-pink-500">
+<div class="w-full h-screen bg-linear-to-b from-purple-500 to-black">
+  <enhanced:img src={logo} />
 </div>
-<div class="container mx-auto">
+<div id="story" class="flex place-content-center bg-fuchsia-900 h-dvh mask-radial-at-center mask-radial-from-10%">
+  <div class="self-center align-middle items-center text-2xl text-shadow-[0_10px_10px_rgb(0_0_0/0.5)]">
+    <h1 class="font-bold my-4 text-6xl italic">STORY</h1>
+    <p class="mb-4">
+      次元の挟間に浮かぶ奇妙な店ーーその名もVortexX。<br/>
+      其処は、時空の歪みに迷い込んだ者にしか辿り着けない幻の場所。
+    </p>
+    <p class="mb-4">
+      ありとあらゆる次元の狭間を漂いながら、訪ねる者を静かに迎え入れる。<br/>
+      VortexXに住まうのは、各々の目的や信念のため、<br/>
+      自らの意思でここに在住することを選んだ様々な者たち。
+    </p>
+    <p class="mb-4">
+      帰還船が到着するまでの間、貴方は彼らとともにひと時の奇妙な時間を共にする。<br/>
+      この店では、あなた自身も彼らと物語を紡ぐ登場人物の一人となる。<br/>
+      あなたはこの店で、どんな<span class="text-red-500 font-bold">出会い</span>と<span class="text-blue-500 font-bold">体験</span>をするのか？
+    </p>
+    <p>ーー今宵、次元の渦がまたひとつ開く。</p>
+  </div>
+</div>
+<div id="info" class="flex place-content-center h-dvh">
+  Info
+</div>
+<div id="members" class="flex place-content-center h-dvh">
   {#each data.cast as cast (cast.name)}
-    <ul>
-      <li>
-        {cast.name}
-      </li>
-    </ul>
+  <ul>
+    <li>
+      {cast.name}
+    </li>
+  </ul>
   {/each}
+</div>
+<div id="videos" class="flex place-content-center h-dvh">
+  Videos
+</div>
+<div id="gallery" class="flex place-content-center h-dvh">
+  Gallery
 </div>
