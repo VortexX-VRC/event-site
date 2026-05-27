0fc187ee203945c852651466674bc538b7794686
Francis Ng <github@fnha.anonaddy.com>
2026-05-27 12:29:04 +0900

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     tailwindcss(),
     enhancedImages(),
     sveltekit()
-  ]
-});
+  ],
+  resolve: {
+    alias: {
+      $img: mode === 'dev' ? '/images' : '/event-site/images'
+    }
+  }
+}));
